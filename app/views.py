3ac43e3e58c8b025f78761c80a94734800f35037
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-06-15 09:21:09 +0500

    hhh

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def indexView(request):
     return render(request, "index.html")
 def aboutView(request):
     return render(request, "about.html")
 
-# Create your views here.
+
